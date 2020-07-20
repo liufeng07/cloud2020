46655a7bb5b3a35d0af07ef8e7df6c6e8f544674
liuf <liufengduke@163.com>
2020-07-20 10:31:42 +0800

    增加单机版Eureka

--- cloud-eureka-server7001/src/main/java/com/itfeng/springcloud/EurekaMain7001.java
+++ cloud-eureka-server7001/src/main/java/com/itfeng/springcloud/EurekaMain7001.java
@@ -2,17 +2,17 @@ package com.itfeng.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @author liuf
- * @Description 支付模块启动类
- * @Date 2020/7/18
+ * @Description TODO
+ * @Date 2020/7/20
  */
+@EnableEurekaServer
 @SpringBootApplication
-@EnableEurekaClient
-public class PaymentMain8001 {
+public class EurekaMain7001 {
     public static void main(String[] args) {
-        SpringApplication.run(PaymentMain8001.class, args);
+        SpringApplication.run(EurekaMain7001.class,args);
     }
 }
