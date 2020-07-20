665299a256d95bc68bc39fda5e72143484df7a2a
liuf <liufengduke@163.com>
2020-07-20 15:12:00 +0800

    增加RestTemplate的get/postForEntity调用

--- cloud-consumer-order80/src/main/java/com/itfeng/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/itfeng/springcloud/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.itfeng.springcloud.controller;
 import com.itfeng.springcloud.entity.CommonResult;
 import com.itfeng.springcloud.entity.Payment;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,13 +24,28 @@ public class OrderController {
     @Resource
     private RestTemplate restTemplate;
 
-    @GetMapping(value = "/consumer/create")
+    @GetMapping(value = "/consumer/payment/create")
     public CommonResult<Payment> insertPayment(Payment payment) {
         return restTemplate.postForObject(PAYMENT_URL + "/payment/insertPayment", payment, CommonResult.class);
     }
 
-    @GetMapping(value = "/consumer/get/{id}")
+    @GetMapping(value = "/consumer/payment/get/{id}")
     public CommonResult<Payment> getPayment(@PathVariable("id") Long id) {
         return restTemplate.getForObject(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+    }
+
+    /**
+     * 使用 getForEntity 返回详细的内容写法
+     *
+     * @param id
+     * @return
+     */
+    @GetMapping("/consumer/payment/getForEntity/{id}")
+    public CommonResult<Payment> getPayment2(@PathVariable("id") Long id) {
+        ResponseEntity<CommonResult> entity = restTemplate.getForEntity(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+        if (entity.getStatusCode().is2xxSuccessful()) {
+            return entity.getBody();
+        }
+        return new CommonResult<>(400, "find fail");
     }
 }
