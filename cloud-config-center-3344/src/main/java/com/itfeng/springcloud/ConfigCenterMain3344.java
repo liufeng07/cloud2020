1fef702e5f04e99d39a2994de32820ee56c8130a
liuf <liufengduke@163.com>
2020-07-21 10:01:21 +0800

    Config配置中心搭建

---
+++ cloud-config-center-3344/src/main/java/com/itfeng/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,18 @@
+package com.itfeng.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author liuf
+ * @Description 配置中心 从github获取配置文件
+ * @Date 2020/7/21
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class,args);
+    }
+}
