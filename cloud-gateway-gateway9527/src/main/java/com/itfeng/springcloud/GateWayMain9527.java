55a5c3c8de843c40a443abdb2ba90f1333c9a93d
liuf <liufengduke@163.com>
2020-07-21 08:55:27 +0800

    GateWay-服务模块搭建

---
+++ cloud-gateway-gateway9527/src/main/java/com/itfeng/springcloud/GateWayMain9527.java
@@ -0,0 +1,18 @@
+package com.itfeng.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author liuf
+ * @Description TODO
+ * @Date 2020/7/21
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GateWayMain9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayMain9527.class,args);
+    }
+}
